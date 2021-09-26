8fb6216ad00512b1af5b3f32119a57bef3f1cb01
Sławomir Woźniak <slawomirw7@hotmail.com>
2021-09-26 12:44:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EvenOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczby parzyste i nieparzyste");
+            Console.WriteLine("Podaj liczbę całkowitą:");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    if (number % 2 == 0)
+                        Console.WriteLine("Liczba parzysta");
+                    else
+                        Console.WriteLine("Liczba nieparzysta");
+
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("\nWprowadzono niepoprawne dane. Podaj liczbę całkowitą:");
+                }
+            }
+        }
+    }
+}
